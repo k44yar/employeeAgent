e97ee8e524dd53556b18d097c737d54daa06b712
Abu <sufian-786@live.co.uk>
2014-12-29 21:02:16 +0000

    third commit - here I have enhanced the user interface and the functionality. I have connected to a database, added data to the tables - and also managed to run the query and show the results in a ListBox. I have an selected employee id field, which allows the user to type in the ID of the employee they want to pull out a record off. The next commit should aim to fix the issue of trying to parse words in the two fields in the form application.

--- EmployeePerformanceSystem/EmployeePerformanceSystem/Form1.Designer.cs
+++ EmployeePerformanceSystem/EmployeePerformanceSystem/Form1.Designer.cs
@@ -32,14 +32,26 @@
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.time_label = new System.Windows.Forms.Label();
+            this.time_entered_label = new System.Windows.Forms.Label();
             this.punctual_time_label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emp_id_field = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ID_Selected_label = new System.Windows.Forms.Label();
+            this.show_data_button = new System.Windows.Forms.Button();
+            this.lbxEmployees = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.search_emp_field = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 12);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(25, 20);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 38);
+            this.button1.Size = new System.Drawing.Size(140, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Employee Entered";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,29 +65,148 @@
             // 
             this.time_label.AutoSize = true;
             this.time_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.time_label.Location = new System.Drawing.Point(217, 20);
+            this.time_label.Location = new System.Drawing.Point(352, 21);
             this.time_label.Name = "time_label";
-            this.time_label.Size = new System.Drawing.Size(39, 20);
+            this.time_label.Size = new System.Drawing.Size(21, 20);
             this.time_label.TabIndex = 1;
-            this.time_label.Text = "time";
+            this.time_label.Text = "...";
+            // 
+            // time_entered_label
+            // 
+            this.time_entered_label.AutoSize = true;
+            this.time_entered_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.time_entered_label.Location = new System.Drawing.Point(644, 61);
+            this.time_entered_label.Name = "time_entered_label";
+            this.time_entered_label.Size = new System.Drawing.Size(21, 20);
+            this.time_entered_label.TabIndex = 2;
+            this.time_entered_label.Text = "...";
+            this.time_entered_label.Click += new System.EventHandler(this.punctal_time_label_Click);
             // 
             // punctual_time_label
             // 
             this.punctual_time_label.AutoSize = true;
             this.punctual_time_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.punctual_time_label.Location = new System.Drawing.Point(429, 20);
+            this.punctual_time_label.Location = new System.Drawing.Point(643, 20);
             this.punctual_time_label.Name = "punctual_time_label";
-            this.punctual_time_label.Size = new System.Drawing.Size(183, 20);
-            this.punctual_time_label.TabIndex = 2;
-            this.punctual_time_label.Text = "Punctual Time:  09:00:00";
-            this.punctual_time_label.Click += new System.EventHandler(this.punctal_time_label_Click);
+            this.punctual_time_label.Size = new System.Drawing.Size(21, 20);
+            this.punctual_time_label.TabIndex = 3;
+            this.punctual_time_label.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(530, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Punctual Time: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(247, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Current Time: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(530, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Time Entered: ";
+            // 
+            // emp_id_field
+            // 
+            this.emp_id_field.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.emp_id_field.Location = new System.Drawing.Point(355, 59);
+            this.emp_id_field.Name = "emp_id_field";
+            this.emp_id_field.Size = new System.Drawing.Size(62, 26);
+            this.emp_id_field.TabIndex = 7;
+            this.emp_id_field.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(247, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Employee ID: ";
+            // 
+            // ID_Selected_label
+            // 
+            this.ID_Selected_label.AutoSize = true;
+            this.ID_Selected_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ID_Selected_label.Location = new System.Drawing.Point(531, 103);
+            this.ID_Selected_label.Name = "ID_Selected_label";
+            this.ID_Selected_label.Size = new System.Drawing.Size(93, 20);
+            this.ID_Selected_label.TabIndex = 9;
+            this.ID_Selected_label.Text = "ID Selected";
+            // 
+            // show_data_button
+            // 
+            this.show_data_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.show_data_button.Location = new System.Drawing.Point(25, 136);
+            this.show_data_button.Name = "show_data_button";
+            this.show_data_button.Size = new System.Drawing.Size(140, 38);
+            this.show_data_button.TabIndex = 12;
+            this.show_data_button.Text = "Show Employee Data";
+            this.show_data_button.UseVisualStyleBackColor = true;
+            this.show_data_button.Click += new System.EventHandler(this.show_data_button_Click);
+            // 
+            // lbxEmployees
+            // 
+            this.lbxEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxEmployees.FormattingEnabled = true;
+            this.lbxEmployees.ItemHeight = 16;
+            this.lbxEmployees.Location = new System.Drawing.Point(26, 193);
+            this.lbxEmployees.Name = "lbxEmployees";
+            this.lbxEmployees.Size = new System.Drawing.Size(942, 36);
+            this.lbxEmployees.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(196, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Select Employee ID: ";
+            // 
+            // search_emp_field
+            // 
+            this.search_emp_field.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.search_emp_field.Location = new System.Drawing.Point(354, 142);
+            this.search_emp_field.Name = "search_emp_field";
+            this.search_emp_field.Size = new System.Drawing.Size(62, 26);
+            this.search_emp_field.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 504);
+            this.ClientSize = new System.Drawing.Size(994, 639);
+            this.Controls.Add(this.search_emp_field);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbxEmployees);
+            this.Controls.Add(this.show_data_button);
+            this.Controls.Add(this.ID_Selected_label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.emp_id_field);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.punctual_time_label);
+            this.Controls.Add(this.time_entered_label);
             this.Controls.Add(this.time_label);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -90,7 +221,18 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label time_label;
+        private System.Windows.Forms.Label time_entered_label;
         private System.Windows.Forms.Label punctual_time_label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox emp_id_field;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label ID_Selected_label;
+        private System.Windows.Forms.Button show_data_button;
+        private System.Windows.Forms.ListBox lbxEmployees;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox search_emp_field;
     }
 }
 
--- EmployeePerformanceSystem/EmployeePerformanceSystem/Form1.cs
+++ EmployeePerformanceSystem/EmployeePerformanceSystem/Form1.cs
@@ -7,150 +7,293 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlServerCe;
 
 namespace EmployeePerformanceSystem
 {
         // --- ASK - Everything you write has to be with in "public partial class Form1 : Form {}"
-        public partial class Form1 : Form
+    public partial class Form1 : Form
+    {
+        SqlCeConnection mySqlConnection; //     --- ASK - For sql connection
+
+        // GLOBAL VARIABLES                                       --- ASK - Global Varaibales being assigned
+        private DateTime time_entered; // the time the employee signed in
+
+        private DateTime punctual_time = new DateTime(0001, 01, 01, 09, 2, 00); //make this into a const after you find 
+        //out the format to set it to 9am everyday.
+
+        private DateTime current_time; //this captures the current time and date
+
+        private bool ontime; //is the user on time? yes/no? (true, false)
+
+        private string CDAY;
+        private int cday;
+        private string PDAY;
+        private int pday;
+
+        private string CMONTH;
+        private int cmonth;
+        private string PMONTH;
+        private int pmonth;
+
+        private string CYEAR;
+        private int cyear;
+        private string PYEAR;
+        private int pyear;
+
+        private string CHOURS;
+        private int chours;
+        private string PHOURS;
+        private int phours;
+
+        private string CMINUTES;
+        private int cminutes;
+        private string PMINUTES;
+        private int pminutes;
+
+        private string CSECONDS;
+        private int cseconds;
+        private string PSECONDS;
+        private int pseconds;
+
+        private string ENTERED_SECONDS;
+        private int entered_seconds;
+
+        private string ENTERED_MINUTES;
+        private int entered_minutes;
+
+        private string ENTERED_HOURS;
+        private int entered_hours;
+
+        private string EMP_ID_STRING;
+        private int emp_id_number;
+
+        private string SELECT_EMP_ID_STRING;
+        private int select_emp_id_number;
+
+
+        /*
+            int year = moment.Year;
+            int month;
+            int day;
+            int hour;
+            int minute;
+            int second;
+        */
+
+        // --- ASK - This method is the first method run when the program starts. STARTING POINT
+        public Form1()
         {
-            // GLOBAL VARIABLES                                       --- ASK - Global Varaibales being assigned
-                private DateTime time_entered; // the time the employee signed in
+            InitializeComponent();
 
-                private DateTime punctual_time = new DateTime(0001, 01, 01, 0, 0, 0); //make this into a const after you find 
-                                                //out the format to set it to 9am everyday.
+            //populateListBox();
 
-                private DateTime current_time; //this captures the current time and date
+            timer1.Start(); // starts timer
 
-                private bool ontime; //is the user on time? yes/no? (true, false)
+            this.punctual_time_label.Text = punctual_time.ToString("HH:mm:ss");
 
-                private string CDAY;
-                private int cday;
-                private string PDAY;
-                private int pday;
-
-                private string CMONTH;
-                private int cmonth;
-                private string PMONTH;
-                private int pmonth;
-
-                private string CYEAR;
-                private int cyear;
-                private string PYEAR;
-                private int pyear;
-
-                private string CHOURS;
-                private int chours;
-                private string PHOURS;
-                private int phours;
-
-                private string CMINUTES;
-                private int cminutes;
-                private string PMINUTES;
-                private int pminutes;
-
-                private string CSECONDS;
-                private int cseconds;
-                private string PSECONDS;
-                private int pseconds;
-                
-
-
-            /*
-                int year = moment.Year;
-	            int month;
-	            int day;
-	            int hour;
-	            int minute;
-                int second;
-            */          
-            
-                 // --- ASK - This method is the first method run when the program starts. STARTING POINT
-                public Form1()
-                {
-                    InitializeComponent();
-
-                    timer1.Start(); // starts timer
-
-                }
-
-                private void button1_Click(object sender, EventArgs e)
-                {
-                    //MAKING all the 'pdates' the same as 'cdates'
-                    PDAY = CDAY;
-                    pday = cday;
-
-                    PMONTH = CMONTH;
-                    pmonth = cmonth;
-
-                    PYEAR = CYEAR;
-                    pyear = cyear;
-
-
-                    // SET PUNCTUAL TIME
-                    PHOURS = current_time.ToString("HH");
-                    phours = Convert.ToInt32(PHOURS);
-
-                    CMINUTES = current_time.ToString("mm");
-                    cminutes = Convert.ToInt32(CMINUTES);
-
-                    PSECONDS = current_time.ToString("ss");
-                    pseconds = Convert.ToInt32(PSECONDS);
-
-                    //this.punctual_time_label.Text = current_time.ToString("HH:mm:ss");
-
-
-                    this.punctual_time_label.Text = pseconds.ToString(); //TESTING PURPOSE
-
-
-                    //here use DateTime time_entered to find the current time
-                    if(time_entered <= punctual_time)
-                    {      
-                        // this will check if the employee is late, if yes then this method will update the LATE row 
-                        //in the database EMPLOYEE table
-
-                        /* the receptionist can use IT. click the button which takes DateTime as a variable and check if its after
-                         9am to check if the employee is late*/
-
-                        /*YOU NEED TO FIND A WAY IN WHICH YOU CAN MATCH THE EMPLOYEE TO THE LATES
-                          MAYBE BY MAKING A FIELD WHICH TAKES EMP_ID, AND ONCE THIS HAS BEEN ENTERED
-                          THEN THIS BUTTON IS VISIBLE TO THE USER*/
-                    }
-                }
-
-                private void timer1_Tick(object sender, EventArgs e)
-                {
-                    // FOR CURRENT TIME
-                    current_time = DateTime.Now;
-
-                    CDAY = current_time.ToString("dd");
-                    cday = Convert.ToInt32(CDAY);
-
-                    CMONTH = current_time.ToString("mm");
-                    cmonth = Convert.ToInt32(CMONTH);
-
-                    CYEAR = current_time.ToString("yyyy");
-                    cyear = Convert.ToInt32(CYEAR);
-
-                    CHOURS = current_time.ToString("HH");
-                    chours = Convert.ToInt32(CHOURS);
-                                
-                    CMINUTES = current_time.ToString("mm");
-                    cminutes = Convert.ToInt32(CMINUTES);
-
-                    CSECONDS = current_time.ToString("ss");
-                    cseconds = Convert.ToInt32(CSECONDS);
-                    
-                    
-                    this.time_label.Text = current_time.ToString("HH:mm:ss");
-
-                    //this.time_label.Text = cminutes.ToString(); //TESTING PURPOSE
-
-                }
-
-                private void punctal_time_label_Click(object sender, EventArgs e)
-                {
-                    //this.punctual_time_label.Text = punctual_time.ToString();
-                }
-                
         }
+
+
+
+        public void populateListBox()
+        {
+            mySqlConnection =
+                    new SqlCeConnection(@"Data Source=C:\Users\Sufian\AppData\Local\EmployeeDatabase.sdf");
+
+            //String selcmd = "SELECT * FROM employee ORDER BY emp_id";
+
+            String selcmd = "SELECT * FROM employee WHERE emp_id =" + select_emp_id_number + " ORDER BY emp_id";
+
+            SqlCeCommand mySqlCommand = new SqlCeCommand(selcmd, mySqlConnection);
+
+            try
+            {
+                mySqlConnection.Open();
+
+                SqlCeDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+                lbxEmployees.Items.Clear();
+
+                while (mySqlDataReader.Read())
+                {
+
+                    lbxEmployees.Items.Add("ID: " + mySqlDataReader["emp_id"] + "   Name: " + mySqlDataReader["name"] + "   Job Role: " + mySqlDataReader["job_role"] + "   Start Date " + mySqlDataReader["start_date"] + "   Late: " + mySqlDataReader["late"] + "   Explained Absences: " + mySqlDataReader["explained_absences"] + "   Unexplained Absences: " + mySqlDataReader["unexplained_absences"]);
+
+                }
+            }
+
+            catch (SqlCeException ex)
+            {
+
+                MessageBox.Show(" .." + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public bool checkInputs()
+        {
+            bool rtnvalue = true;
+
+            if (string.IsNullOrEmpty(search_emp_field.Text))
+            {
+                MessageBox.Show("Error: Please check your inputs");
+                rtnvalue = false;
+            }
+
+            return (rtnvalue);
+        }
+
+
+        /*public void insertRecord(String ID, String name, String address, String commandString)
+        {
+
+            try
+            {
+                SqlCeCommand cmdInsert = new SqlCeCommand(commandString, mySqlConnection);
+
+                cmdInsert.Parameters.AddWithValue("@ID", ID);
+                cmdInsert.Parameters.AddWithValue("@name", name);
+                cmdInsert.Parameters.AddWithValue("@address", address);
+                cmdInsert.ExecuteNonQuery();
+            }
+            catch (SqlCeException ex)
+            {
+                MessageBox.Show(ID + " .." + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }*/
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.punctual_time_label.Text = current_time.ToString("HH:mm:ss");
+
+            EMP_ID_STRING = emp_id_field.Text;
+            emp_id_number = Convert.ToInt32(EMP_ID_STRING);
+
+            this.ID_Selected_label.Text = EMP_ID_STRING;
+
+            //MAKING all the 'pdates' the same as 'cdates'
+            PDAY = CDAY;
+            pday = cday;
+
+            PMONTH = CMONTH;
+            pmonth = cmonth;
+
+            PYEAR = CYEAR;
+            pyear = cyear;
+
+            entered_hours = chours;
+
+            entered_minutes = cminutes;
+
+            entered_seconds = cseconds;
+
+
+
+            // SET PUNCTUAL TIME
+            PHOURS = punctual_time.ToString("HH");
+            phours = Convert.ToInt32(PHOURS);
+
+            CMINUTES = punctual_time.ToString("mm");
+            cminutes = Convert.ToInt32(CMINUTES);
+
+            PSECONDS = punctual_time.ToString("ss");
+            pseconds = Convert.ToInt32(PSECONDS);
+
+
+
+            this.time_entered_label.Text = current_time.ToString("HH:mm:ss");
+            // this.punctual_time_label.Text = pseconds.ToString(); //TESTING PURPOSE
+
+
+            //here use DateTime time_entered to find the current time
+            if (phours <= entered_hours)
+            {
+                if (phours <= entered_hours && pminutes <= entered_minutes)
+                {
+
+                }
+
+
+                // this will check if the employee is late, if yes then this method will update the LATE row 
+                //in the database EMPLOYEE table
+
+                /* the receptionist can use IT. click the button which takes DateTime as a variable and check if its after
+                 9am to check if the employee is late*/
+
+                /*YOU NEED TO FIND A WAY IN WHICH YOU CAN MATCH THE EMPLOYEE TO THE LATES
+                  MAYBE BY MAKING A FIELD WHICH TAKES EMP_ID, AND ONCE THIS HAS BEEN ENTERED
+                  THEN THIS BUTTON IS VISIBLE TO THE USER*/
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // FOR CURRENT TIME
+            current_time = DateTime.Now;
+
+            CDAY = current_time.ToString("dd");
+            cday = Convert.ToInt32(CDAY);
+
+            CMONTH = current_time.ToString("mm");
+            cmonth = Convert.ToInt32(CMONTH);
+
+            CYEAR = current_time.ToString("yyyy");
+            cyear = Convert.ToInt32(CYEAR);
+
+            CHOURS = current_time.ToString("HH");
+            chours = Convert.ToInt32(CHOURS);
+
+            CMINUTES = current_time.ToString("mm");
+            cminutes = Convert.ToInt32(CMINUTES);
+
+            CSECONDS = current_time.ToString("ss");
+            cseconds = Convert.ToInt32(CSECONDS);
+
+
+            this.time_label.Text = current_time.ToString("HH:mm:ss");
+
+            //this.time_label.Text = cminutes.ToString(); //TESTING PURPOSE
+
+        }
+
+        private void punctal_time_label_Click(object sender, EventArgs e)
+        {
+            //this.punctual_time_label.Text = punctual_time.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //nothing yet
+
+            //this.emp_id.Text = "hello"; //TESTING PURPOSE to see what this does
+        }
+
+        private void show_data_button_Click(object sender, EventArgs e)
+        {
+               
+            if (checkInputs())
+            {
+                SELECT_EMP_ID_STRING = search_emp_field.Text;
+                select_emp_id_number = Convert.ToInt32(SELECT_EMP_ID_STRING);
+                
+                //String commandString = "INSERT INTO tblstudent(studentID, studentNAME, studentADDRESS) VALUES (@ID, @name, @address)";
+
+                //insertRecord(txtId.Text, txtName.Text, txtAddress.Text, commandString);
+                populateListBox();
+                //cleartxtBoxes();
+            }
+
+        }
+
+
+
+    }
 }
